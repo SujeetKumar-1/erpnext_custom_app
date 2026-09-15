74a6de5fd70de4c0cf50bd5744670994e1b81b7f
SujeetKumar-1 <sujeetkumar01357@gmail.com>
2026-09-15 23:15:56 +0530

    feat: Initialize App

---
+++ README.md
@@ -0,0 +1,7 @@
+## Employee Exit Automation
+
+Handle Employee resignation and document handovers
+
+#### License
+
+MIT
---
+++ employee_exit_automation/config/desktop.py
@@ -0,0 +1,10 @@
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "Employee Exit Automation",
+			"type": "module",
+			"label": _("Employee Exit Automation")
+		}
+	]
